7dc60c2aa6fdd47d3b0410248c583c56ce8a66b0
Thiago Boeno <thiagoboeno@dialhost.com.br>
2021-11-03 18:40:18 -0300

    Initial structure

---
+++ routes/posts.js
@@ -0,0 +1,93 @@
+const router = require('express').Router();
+const Post = require('../models/Post');
+const User = require('../models/User');
+
+router.post('/', async (req, res) => {
+  const newPost = new Post(req.body);
+  
+  try {
+    const savedPost = await newPost.save();
+    
+    return res.status(200).json(savedPost);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (post.userId === req.body.userId) {
+      await post.updateOne({ $set: req.body });
+
+      return res.status(200).json('Your post has been updated');
+    } else {
+      return res.status(403).json('You can update only your post');
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+    if (post.userId === req.body.userId) {
+      await post.deleteOne();
+
+      res.status(200).json('Your post has been deleted');
+    } else {
+      res.status(403).json('You can delete only your post');
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.put('/:id/like', async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if (!post.likes.includes(req.body.userId)) {
+      await post.updateOne({ $push: { likes: req.body.userId } });
+
+      return res.status(200).json('The post has been liked');
+    } else {
+      await post.updateOne({ $pull: { likes: req.body.userId } });
+
+      return res.status(200).json('The post has been disliked');
+    }
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.get('/timeline/all', async (req, res) => {
+  try {
+    const currentUser = await User.findById(req.body.userId);
+    const userPosts = await Post.find({ userId: currentUser._id });
+    const friendPosts = await Promise.all(
+      currentUser.followings.map((friendId) => {
+        return Post.find({ userId: friendId });
+      })
+    );
+    const timeline = userPosts.concat(...friendPosts).sort((a,b) => b.updatedAt - a.updatedAt);
+
+    return res.json(timeline);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
+});
+
+module.exports = router;
---
+++ routes/users.js
@@ -0,0 +1,99 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const bcrypt = require('bcrypt');
+
+router.get('/:id', async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    const { password, updatedAt, ...other } = user._doc;
+
+    return res.status(200).json(other);
+  } catch (error) {
+    return res.status(500).json(error);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    if (req.body.password) {
+      try {
+        const salt = await bcrypt.genSalt(10);
+        req.body.password = await bcrypt.hash(req.body.password, salt);
+      } catch (error) {
+        return res.status(500).json(error);
+      } 
+    }
+
+    try {
+      const user = await User.findByIdAndUpdate(req.params.id, {
+        $set: req.body,
+      });
+
+      return res.status(200).json('Account has been updated');
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json('You can update only your account!');
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  if (req.body.userId === req.params.id || req.body.isAdmin) {
+    try {
+      await User.findByIdAndDelete(req.params.id);
+
+      return res.status(200).json('Account has been deleted');
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json('You can delete only your account!');
+  }
+});
+
+router.put('/:id/follow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (!user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $push: { followers: req.body.userId } });
+        await currentUser.updateOne({ $push: { followings: req.params.id } });
+
+        return res.status(200).json('User has been followed');
+      } else {
+        return res.status(403).json('You already follow this user');
+      }
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json('You can\'t follow yourself!');
+  }
+});
+
+router.put('/:id/unfollow', async (req, res) => {
+  if (req.body.userId !== req.params.id) {
+    try {
+      const user = await User.findById(req.params.id);
+      const currentUser = await User.findById(req.body.userId);
+
+      if (user.followers.includes(req.body.userId)) {
+        await user.updateOne({ $pull: { followers: req.body.userId } });
+        await currentUser.updateOne({ $pull: { followings: req.params.id } });
+
+        return res.status(200).json('User has been unfollowed');
+      } else {
+        return res.status(403).json('You already unfollow this user');
+      }
+    } catch (error) {
+      return res.status(500).json(error);
+    }
+  } else {
+    return res.status(403).json('You can\'t unfollow yourself!');
+  }
+});
+
+module.exports = router;
